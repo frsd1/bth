7cd33347cc26fcca7ce11a3a459af6b96ec082b0
Fred <your_email@whatever.com>
2014-09-16 12:29:39 +0200

    tar bort javascript1

--- bth/dbwebb/javascript1/mall/index.php
+++
@@ -1,7 +0,0 @@
-<?php $title='Javascript på BTH - Kursmoment 1'; include(__DIR__ . '/../mall/header.php'); ?>
-
-<div id='flash'>
-<p id='text' class='black'>Kurs i javascript på Blekinge Tekniska Högskola</p>
-</div>
- 
-<?php $path=__DIR__; include(__DIR__ . '/../mall/footer.php'); ?>
